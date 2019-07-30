837b01c7e7014abfc26df95a4eaa25cdfcdcb835
smokyfog <1473092739@qq.com>
2019-07-30 23:49:17 +0800

    上传小视频的逻辑

--- routes/admin/smallvideo.js
+++ routes/admin/smallvideo.js
@@ -1,14 +1,36 @@
 const router = require('koa-router')()
+const comm = require('../../comm/comm')
 const SmallVideo = require('../../model/admin/smallvideo')
 
 router.prefix('/smallvideo')
 
 router.post('/add', async (ctx) => {
-    let data = ctx.request.body
-    ctx.body = {
-        code: -1,
-        data: data,
-        message: ''
+    let { path, name, score, basicInfo, plotDesc, trailer, cover, userid, praiseCount, type } = ctx.request.body
+    let smallvideo = new SmallVideo({
+        path,
+        name,
+        score,
+        basicInfo,
+        plotDesc,
+        userid,
+        praiseCount,
+        type,
+        trailer: comm.getUrlDir(cover),
+        cover: comm.getUrlDir(cover),
+    })
+    let result = await smallvideo.save()
+    if (result) {
+        ctx.body = {
+            code: 0,
+            data: result,
+            message: '上传成功'
+        }
+    } else {
+        ctx.body = {
+            code: -1,
+            data: [],
+            message: '上传失败'
+        }
     }
 })
 
